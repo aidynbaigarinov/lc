8b15d7f0891058810e35082cfedb798cd891d444
aidynbaigarinov <aidyn.baigarinov@gmail.com>
2020-07-01 11:26:21 +0600

    lc

---
+++ string/Reverse_string_344.go
@@ -0,0 +1,15 @@
+package string
+
+// ReverseString reverses the array of bytes
+func ReverseString(s []byte) {
+	if len(s) == 0 {
+		return
+	}
+	var low, hi = 0, len(s)-1
+
+	for low < hi {
+		s[low], s[hi] = s[hi], s[low]
+		low++
+		hi--
+	}
+}
